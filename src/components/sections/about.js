37a32532514f916fe23348240ed7ea36b0c51199
Ankur Pandey <pankur216@gmail.com>
2025-01-29 16:30:45 -0500

    removed aif

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -180,7 +180,8 @@ const About = () => {
               src="../../images/me.jpg"
               width={500}
               quality={95}
-              formats={['AUTO', 'WEBP', 'AVIF']}
+              // formats={['AUTO', 'WEBP', 'AVIF']}
+              formats={['AUTO', 'WEBP']}
               alt="Headshot"
             />
           </div>
